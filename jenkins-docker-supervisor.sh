d92ba100b8cea49b2528c641ac6ec43a035186e9
Aleks Saul <aleks.saul@coreos.com>
2016-08-02 12:09:48 -0400

    update for dockercfg

--- jenkins-docker-supervisor.sh
+++ jenkins-docker-supervisor.sh
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 if [ -a /root/.dockercfg ]; then
-  cp /root/.dockercfg /home/jenkins-agent/
+  cp /root/dockercfg $HOME/.dockercfg
 fi
 
 
